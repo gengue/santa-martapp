57146830f8704c98af299cf08b46d15eef3e1c45
Genesis Guerrero Martinez <genesisdaft@gmail.com>
2014-05-20 14:37:24 -0500

    agregado fastClick y menu desplegable

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var app = {
+    // Constructor de la app
+    initialize: function() {
+    	alert("entra");
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    onDeviceReady: function() {
+    	// Ejecutamos la funci—n FastClick, que es la que nos elimina esos 300ms de espera al hacer click
+    	new FastClick(document.body);
+    },
+    
+};
